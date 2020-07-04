49d5b407bde4688c7646c527904ce6ad9993216f
Vitaliy_B <vlvlv.vovov@bk.ru>
2020-07-04 09:30:19 +0500

    build

---
+++ build/precache-manifest.e43fad6f374f157a45a027bceda62eb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31791469205654f4f8561d08b78c5888",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad13940ff6c212c183ca",
+    "url": "/static/css/main.11b67816.chunk.css"
+  },
+  {
+    "revision": "49c3e79b91d9e4860cd7",
+    "url": "/static/js/2.29e16678.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29e16678.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad13940ff6c212c183ca",
+    "url": "/static/js/main.3b45f5c1.chunk.js"
+  },
+  {
+    "revision": "2201980838728cdea7c4",
+    "url": "/static/js/runtime-main.022a23b8.js"
+  }
+]);
